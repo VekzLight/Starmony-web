621ff9d59d6cb80a7caf7ba2dcac6366615bd856
VekzLight <DNASystem.vekz@gmail.com>
2022-08-03 22:44:04 -0500

    Limpia del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,44 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { GeneratorComponent } from './pages/generator/generator.component';
-import { RecognizerComponent } from './pages/recognizer/recognizer.component';
-import { CreatorComponent } from './pages/creator/creator.component';
-
-import { RecognizerChordComponent } from './components/recognizer-chord/recognizer-chord.component';
-import { RecognizerScaleComponent } from './components/recognizer-scale/recognizer-scale.component';
-import { RecognizerIntervalComponent } from './components/recognizer-interval/recognizer-interval.component';
-import { RecognizerProgressionComponent } from './components/recognizer-progression/recognizer-progression.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { CollectionComponent } from './pages/collection/collection.component';
-import { CreatorScaleComponent } from './components/creator-scale/creator-scale.component';
-import { CreatorProgressionComponent } from './components/creator-progression/creator-progression.component';
 import { SeekerComponent } from './components/seeker/seeker.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent,
       children:[
-        { path: 'seeker', component: SeekerComponent},
-        { path: 'collection', component: CollectionComponent}
+        { path: 'seeker', component: SeekerComponent}
       ]},
-  { path: 'login', component: LoginComponent},
-  { path: 'generator', component: GeneratorComponent},
-  { path: 'recognizer', component: RecognizerComponent,
-      children:[
-        { path: 'chord', component: RecognizerChordComponent},
-        { path: 'scale', component: RecognizerScaleComponent},
-        { path: 'interval', component: RecognizerIntervalComponent},
-        { path: 'progression', component: RecognizerProgressionComponent},
-      ]},
-  { path: 'creator', component: CreatorComponent,
-      children:[
-        { path: 'scale', component: CreatorScaleComponent},
-        { path: 'progression', component: CreatorProgressionComponent},
-      ]},
-  { path: 'profile', component: ProfileComponent},
-  { path: 'collection', component: CollectionComponent},
-  { path: '', redirectTo: '/login', pathMatch: 'full'}
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,43 +12,19 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-
-import { RecognizerComponent } from './pages/recognizer/recognizer.component';
-import { CreatorComponent } from './pages/creator/creator.component';
-import { GeneratorComponent } from './pages/generator/generator.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { RecognizerChordComponent } from './components/recognizer-chord/recognizer-chord.component';
-import { RecognizerScaleComponent } from './components/recognizer-scale/recognizer-scale.component';
-import { RecognizerIntervalComponent } from './components/recognizer-interval/recognizer-interval.component';
-import { RecognizerProgressionComponent } from './components/recognizer-progression/recognizer-progression.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CollectionComponent } from './pages/collection/collection.component';
-import { CreatorScaleComponent } from './components/creator-scale/creator-scale.component';
-import { CreatorProgressionComponent } from './components/creator-progression/creator-progression.component';
-import { SeekerComponent } from './components/seeker/seeker.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { OrderSelectElementComponent } from './components/order-select-element/order-select-element.component';
 import { OrderSelectorDirective } from './directives/order-selector.directive';
 
+import { SeekerComponent } from './components/seeker/seeker.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    RecognizerComponent,
-    CreatorComponent,
-    GeneratorComponent,
-    ProfileComponent,
-    RecognizerChordComponent,
-    RecognizerScaleComponent,
-    RecognizerIntervalComponent,
-    RecognizerProgressionComponent,
-    CollectionComponent,
-    CreatorScaleComponent,
-    CreatorProgressionComponent,
     SeekerComponent,
     OrderSelectElementComponent,
     OrderSelectorDirective
